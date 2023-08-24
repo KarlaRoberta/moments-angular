3f221b7bab8afea6397c24288eae37c15ff568c3
KarlaR <karlarobertati@gmail.com>
2023-08-24 12:27:36 -0300

    inserindo dados no sistema

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,8 @@
+import { MomentService } from './../../../services/moment.service';
 import { Component } from '@angular/core';
+import {Moment} from 'src/app/Moments';
+
+
 
 @Component({
   selector: 'app-new-moment',
@@ -7,5 +11,28 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText= "Compartilhar!";
+
+  constructor(private momentService: MomentService){}
+
+ async createHandler(moment: Moment){
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image){
+      formData.append('image',moment.image);
+
+    }
+
+    //toDo
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    //exibir msg
+
+    //redirect
+
+  }
 
 }
